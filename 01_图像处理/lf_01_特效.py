383bec21432a75ff0b4ad9d1e26cf6288475cdb6
lifei <772404287@qq.com>
2021-11-29 09:54:56 +0800

    'update code'

--- 01_图像处理/lf_01_特效.py
+++ 01_图像处理/lf_01_特效.py
@@ -1,7 +1,17 @@
 import cv2
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
+"""
+Author:lifei
+date:2021/11/28
+"""
+# 毛玻璃特效
+img = cv2.imread('./imagedata/meinv.png')
+img1 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+h,w = img.shape[:2]
+print("图片的高和宽为：", h, w)
+cv2.imshow("W0",img)
+cv2.imshow("W1",img1)
+cv2.waitKey(delay=0)
 
-print('test')
 
